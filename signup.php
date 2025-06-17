8201c6c259c6af7b10b12875ed716c0b4a273462
rehanaja <muhamadraihan1611@gmail.com>
2025-06-18 02:18:36 +0700

    Responsive dan styling about.html

--- signup.php
+++ signup.php
@@ -4,27 +4,37 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>BitBelajar - Sign Up</title>
+  <!-- custom css -->
   <link rel="stylesheet" href="assets\css\signup.css" />
+
+  <!-- font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&family=Teko:wght@300..700&display=swap"
+      rel="stylesheet"
+    />
 </head>
 <body>
   <div class="wrapper">
     <div class="signup-section">
       <div class="brand">
-        <h2>BitBelajar</h2>
+        <h2><a href="index.html">BitBelajar</a></h2>
         <p>Learn Together, Grow Together</p>
       </div>
       <div class="form-container">
         <h3>Sign Up</h3>
         <form>
-          <input type="text" placeholder="First name" required />
-          <input type="text" placeholder="Last name" required />
+          <input type="text" placeholder="Name" required />
+          <input type="text" placeholder="Username" required />
           <input type="email" placeholder="Email address" required />
           <input type="password" placeholder="Password" required />
+          <input type="password" placeholder="Confirm Password" required />
           <label class="terms">
             <input type="checkbox" required />
             Accept Terms and Conditions
           </label>
-          <button type="submit">Join us →</button>
+          <button type="submit">Register →</button>
         </form>
       </div>
     </div>

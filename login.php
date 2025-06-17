d7725b23638dcbf863b6ba0dfe4f0ff5cc2b2045
Muhamad Raihan <muhamadraihan1611@gmail.com>
2025-06-17 18:03:59 +0700

    Perubahan warna dan tambah about.html

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BitBelajar Login</title>
+    <title>BitBelajar - Login</title>
 
     <!-- login style css  -->
     <link rel="stylesheet" href="assets/css/login.css" />
